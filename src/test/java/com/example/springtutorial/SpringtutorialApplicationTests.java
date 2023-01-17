58c32821a27790ab3e75ae4ac898a62e1c190566
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2023-01-17 17:14:53 +0100

    initial commit

---
+++ src/test/java/com/example/springtutorial/SpringtutorialApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtutorialApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
